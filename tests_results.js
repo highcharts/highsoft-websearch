d15ce001b1c8560151e3d7a227c5aa059aafd435
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-10-14 13:50:22 +0200

    Add local server

---
+++ tests_results.js
@@ -0,0 +1,162 @@
+/*!*
+ *
+ *  Copyright (C) Highsoft AS
+ *
+ * */
+
+const ChildProcess = require('child_process');
+const ClientPath = require('path').posix;
+const FS = require('fs');
+const HTTP = require('http');
+const ServerPath = require('path');
+
+const configuration = {
+    baseDirectory: process.cwd(),
+    serverAddress: '127.0.0.1',
+    serverPort: 8000
+};
+
+function onClientError (error, socket) {
+    console.error(error);
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+}
+
+function onError (error) {
+    console.error(error);
+    process.exit(1);
+}
+
+function responseNotFound (response) {
+    console.log('=> 404 Not Found');
+    response.writeHead(404, 'Not Found');
+    response.end();
+}
+
+function responseNotSupported (response) {
+    console.log('=> 405 Method Not Allowed');
+    response.writeHead(405, 'Method Not Allowed');
+    response.end();
+}
+
+function responseRedirect (response, location) {
+    console.log('=> 303 See Other');
+    response.writeHead(303, 'See Other', { 'Location': location });
+    response.end();
+}
+
+function serve (request, response) {
+
+    const baseDirectory = configuration.baseDirectory;
+    const requestedClientPath = request.url;
+    const requestedMethod = request.method;
+    const requestedServerPath = ServerPath.join(baseDirectory, request.url);
+
+    console.info(`${requestedMethod} ${requestedClientPath}`);
+
+    switch (requestedMethod) {
+        default:
+            responseNotSupported(response);
+            return;
+        case 'GET':
+        case 'HEAD':
+            break;
+    }
+
+    if (
+        !requestedServerPath.startsWith(baseDirectory) ||
+        !FS.existsSync(requestedServerPath)
+    ) {
+        responseNotFound(response);
+        return;
+    }
+
+    const requestServerStats = FS.statSync(requestedServerPath);
+
+    let contentLength = requestServerStats.size;
+    let contentType;
+
+    switch (ServerPath.extname(requestedServerPath)) {
+        default: 
+            contentType = 'application/octet-stream';
+            break;
+        case '':
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+        case '.htm':
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.jpg':
+        case '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+        case '.js':
+            contentType = 'application/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.svg':
+            contentType = 'image/svg';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+    }
+
+    if (typeof contentType === 'undefined') {
+        responseRedirect(response, ClientPath.join(requestedClientPath, 'index.html'));
+        return;
+    }
+
+    if (response.finished) {
+        return;
+    }
+
+    response.writeHead(
+        200,
+        {
+            'Content-Length': contentLength,
+            'Content-Type': contentType
+        }
+    );
+
+    if (requestedMethod === 'HEAD') {
+        response.end();
+    }
+    else {
+        response.end(FS.readFileSync(requestedServerPath));
+    }
+}
+
+HTTP
+    .createServer(serve)
+    .on('clientError', onClientError)
+    .on('error', onError)
+    .listen(configuration.serverPort, configuration.serverAddress, () => {
+
+        const host = configuration.serverAddress + ':' + configuration.serverPort;
+        const hostURL = 'http://' + host + '/tests_results.html';
+
+        console.log(`Listening on ${host}...`);
+
+        switch (process.platform) {
+            default:
+                ChildProcess.exec('xdg-open ' + hostURL);
+                break;
+            case 'darwin':
+                ChildProcess.exec('open ' + hostURL);
+                break;
+            case 'win32':
+                ChildProcess.exec('cmd /c start ' + hostURL);
+                break;
+        }
+    });
